59d87ac6665b201d5430218550b49dfe793d74e8
manodhiambo <manodhiambo@gmail.com>
2021-08-26 22:28:15 +0300

    several commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,23 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return; Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(d));
+	printf("Size of an int: %d bytes(s)\n", sizeof(a));
+	printf("Size of a long int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	return (0);
+}
 
