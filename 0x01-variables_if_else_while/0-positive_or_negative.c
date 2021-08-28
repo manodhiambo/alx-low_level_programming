797d694f47eec93a70a5a1176b160b4ddb1344af
manodhiambo <manodhiambo@gmail.com>
2021-08-28 21:40:07 +0300

    committing on updates

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,12 +19,12 @@ if (n > 0)
 }
 else if (n < 0)
 { 
- printf("%d is negative\n" );
+ printf("%d is negative\n" , n);
 
 }
 else 
 {
- printf("%d is zero\n" , );
+ printf("%d is zero\n" , n);
 }
 return (0);
 }
