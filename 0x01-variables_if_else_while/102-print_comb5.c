5873e776d9a04b8c0563f74ecbdad74513b381a8
manodhiambo <manodhiambo@gmail.com>
2021-08-30 15:36:00 +0300

    commits on 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,36 +16,53 @@
 *All your code should be in the main function
 *Return: 0
 */
+
 int main(void)
+
 {
 	int i, j;
+
 	int a, b, c, d;
-	for (i = 0; i < 100; i++)
+
+	for (i = 0; i < 100; i++
+			)
 	{
 		a = i / 10; /* doubles fnum */
+
 		b = i % 10; /* singles fnum */
+
 		for (j = 0; j < 100; j++)
+
 		{
-			c = j / 10 /* doubles fnum */
+			c = j / 10; /* doubles fnum */
+
 				d = j % 10; /* single fnum */
+
 			if (a < c || (a == c && b < d))
+
 			{
 				putchar(a + '0');
+
 				putchar(b + '0');
+
 				putchar(32);
+
 				putchar(c + '0');
+
 				putchar(d + '0');
 
 				if (!(a == 9 && b == 8))
+
 				{
 					putchar(44);
+
 					putchar(32);
 				}
 			}
 		}
 	}
+
 	putchar(10);
 
 	return (0);
 }
-
