80b978dec50e957577d79368b6cf97deceabf859
manodhiambo <manodhiambo@gmail.com>
2021-08-30 15:18:37 +0300

    worked on 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,29 +15,42 @@
 *All your code should be in the main function
 *Return: 0
 */
+
 int main(void)
+
 {
 	int i, j, k, l;
+
 	for (i = 0; i < 1000; i++)
+
 	{
 		j = i / 100; /* hundreds*/
-		k = (i /10) % 10;/* tens*/
+
+		k = (i / 10) % 10;/* tens*/
+
 		l = i % 10; /* singles*/
-		if ( j < k && k < l)
+
+		if (j < k && k < l)
+
 		{
 			putchar(j + '0');
-				putchar(k + '0');
-				putchar(l + '0');
-				if ( i < 700)
+
+				putchar(k + '0' );
+
+				putchar(l + '0' );
+
+				if (i < 700)
+
 				{
-					putchar(44);
-					putchar(32);
+					putchar(44 );
+
+					putchar(32 );
+
 				}
 		}
 	}
 	putchar('\n');
 
+
 	return (0);
 }
-
-
