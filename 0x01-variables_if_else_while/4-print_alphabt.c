4adc32eb1cac64a0fa037dd1a4a9641c3164a6b8
manodhiambo <manodhiambo@gmail.com>
2021-08-30 13:43:29 +0300

    complete error

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,24 @@
  *  Print the alphabet in lowcase expect q and e 
  *Return: 0
  */
+
 int main(void)
+
 {
 	char ch, e, q;
-	e = 'e';
-	q = 'q';
-	for (ch = 'a'; ch <= 'z'; ch++)
-		if (ch != e && ch != q)
-			putchar(ch);
-	putchar('\n');
-return (0);
-}
 
+	e = 'e';
+
+	q = 'q';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		if (ch != e && ch != q)
+
+			putchar(ch);
+
+	putchar('\n');
+
+return (0);
+
+}
