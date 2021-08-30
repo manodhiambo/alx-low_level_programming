de29ea7337f8ce9ad368f69f2ece4a812d1c0fed
manodhiambo <manodhiambo@gmail.com>
2021-08-30 14:59:13 +0300

    commit on 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,33 @@
  * A program that prints all possible combinations of single-digit numbers
  * Return: 0
  */
+
 int main(void)
+
 {
 	int d = 0;
+
 	while (d < 10)
+
 	{
 		putchar(d + '0');
+
 		if (d < 9)
+
 		{
 			putchar(',');
+
 			putchar(' ');
+
 		}
+
 		d++
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
 
