ccfe868007428c0582b39591df7b6f3ef89fa87c
manodhiambo <manodhiambo@gmail.com>
2021-08-30 15:25:17 +0300

    sure on 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,11 +24,11 @@ int main(void)
 	for (i = 0; i < 1000; i++)
 
 	{
-		j = i / 100; /* hundreds*/
+		j = i / 100;
 
-		k = (i / 10) % 10;/* tens*/
+		k = (i / 10) % 10;
 
-		l = i % 10; /* singles*/
+		l = i % 10;
 
 		if (j < k && k < l)
 
