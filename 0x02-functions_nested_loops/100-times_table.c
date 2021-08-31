8d0794c82177462ed7cfcaa3d2b011a925bb1bb5
manodhiambo <manodhiambo@gmail.com>
2021-08-31 21:22:58 +0300

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,12 +4,10 @@
 /**
  * main - main block
  * A function that prints the n times table, starting with 0.
- *Prototype: void print_times_table(int n);
  *If n is greater than 15 or less than 0 the function should not print anything
- *Format: see example
  *Return: nothing
  */
-void times_table(int n)
+void print_times_table(int n)
 
 {
 	int row, col;
