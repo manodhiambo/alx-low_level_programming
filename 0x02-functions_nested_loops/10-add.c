2d3f24c09cba9e58bb956d2ae95a5069ac6d1d63
manodhiambo <manodhiambo@gmail.com>
2021-08-31 20:46:22 +0300

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <time.h>
-#include "holberton"
-
+#include "main.h"
 /**
  * main - main block
  * A function that adds two integers and returns the result.
  *Prototype: int add(int, int)
  *Return: int
  */
-
 int add(int n1, int n2)
-
 {
 	int res;
 
