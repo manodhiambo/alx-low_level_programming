bd66a348577d52c0769ec16ffaa5342a62de83ba
manodhiambo <manodhiambo@gmail.com>
2021-08-31 17:53:29 +0300

    software is the key

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <time.h>
-#include "holberton.h"
 
 /**
  * main - main block
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <time.h>
-#include "holberton"
 
 /**
  * main - main block
