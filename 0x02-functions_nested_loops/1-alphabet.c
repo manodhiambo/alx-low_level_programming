190361a10dbf18194ebb4a1687a8cbe72d652ff9
manodhiambo <manodhiambo@gmail.com>
2021-08-31 08:46:13 +0300

    committing on 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <holberton.h"
+
+/**
+ * main - main block
+ *A function that prints the alphabet, in lowercase, followed by a new line
+ *Prototype: void print_alphabet(void);
+ *You can only use _putchar twice in your code
+ *Return: nothing.
+ */
+
+void print_alphabet(void)
+
+{
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+
+		_putchar(l);
+
+	l += 1;
+
+	}
+
+	_putchar(10);
+
+}
