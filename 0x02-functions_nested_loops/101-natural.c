99805b7d2ab1415be1569ccc8842c77da3bd318a
manodhiambo <manodhiambo@gmail.com>
2021-08-31 21:45:54 +0300

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,14 +4,15 @@
 /**
  * main - main block
  * If we list all the natural numbers below 10 that are multiples of 3 or 5.
- *We 3, 5, 6 and 9. The sum of these multiples is 23. 
- *Return: int
+ * We 3, 5, 6 and 9. The sum of these multiples is 23. 
+ * Return: int
  */
 int main(void)
 {
 	int multi = 0;
 
 	int res = 0;
+
 	while (multi < 1024)
 	{
 		if (multi % 3 == 0 || multi % 5 == 0)
