5acbbf2d4f9bd405c2d5989436c155692c616873
manodhiambo <manodhiambo@gmail.com>
2021-08-31 21:08:57 +0300

    on 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include "holberton"
+#include "main.h"
 #include <time.h>
-
 /**
  * main - main block
  * A function that prints the n times table, starting with 0.
@@ -10,7 +9,6 @@
  *Format: see example
  *Return: nothing
  */
-
 void print_times_table(int n)
 
 {
@@ -19,68 +17,41 @@ void print_times_table(int n)
 	int mul = 0;
 
 	if (n <= 15 && n >= 0)
-
 	{
 		for (row = 0; row <= n; row++)
-
 		{
 			for (col = 0; col <= n; col++)
-
 			{
 				mult = (row * col);
-
 				if (mult < 10)
-
 				{
 					if (col != 0)
-
 					{
 						_putchar(' ');
-
 						_putchar(' ');
-
 						_putchar(' ');
-
 					}
-
 					_putchar(mult + '0');
-
 				}
-
 				else if (mult < 100)
-
 				{
 					_putchar(' ');
-
 					_putchar(' ');
-
 					_putchar((mult / 10) + '0');
-
 					_putchar((mult % 10) + '0');
 				}
-
 				else
-
 				{
 					_putchar(' ');
-
 					_putchar((mult / 100) + '0');
-
 					_putchar(((mult % 100) / 10) + '0');
-
 					_putchar((mult % 10) + '0');
 				}
-
 				if (col != n)
-
 					_putchar(',');
 			}
 			
 			_putchar('\n');
 		}
 	}
-}
-
-				
-
-
+}				
