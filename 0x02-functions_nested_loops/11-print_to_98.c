2a19fe4caaad93b8c4b17a3a9ea4316be2f63c00
manodhiambo <manodhiambo@gmail.com>
2021-08-31 20:54:44 +0300

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,35 @@
 #include <stdio.h>
 #include <time.h>
-#inlude "holberton"
+#include "main.h"
 
 /**
  * main - main block
- * A function that prints all natural numbers from n to 98, followed by a new line.
- *Prototype: void print_to_98(int n);
+ * A function that prints all natural numbers from n to 98, 
  *Numbers must be separated by a comma, followed by a space
  *Numbers should be printed in order
  *The first printed number should be the number passed to your function
  *The last printed number should be 98
- *You are allowed to use the standard library
  *Return: nothing
  */
-
 void print_to_98(int n)
-
 {
 	if (n < 98)
 
 	{
 		for (; n < 98; n++)
-
 		{
 			printf("%d, ", n);
 		}
 	}
-
 	else if (n > 98)
-
 	{
 		for (; n > 98; n--)
-
 		{
-			printf(%d, ", n);
+			printf("%d, ", n);
 		}
 	}
-
 	if (n == 98)
-
 	{
-		printf("%d\n, n);
+		printf("%d\n, ", n);
 	}
 }
-
