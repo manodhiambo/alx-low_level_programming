35fdf4eb7c10541abddface3526d89052ffbbde3
manodhiambo <manodhiambo@gmail.com>
2021-08-31 21:35:48 +0300

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - main block
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we  *get 3, 5, 6 and 9. The sum of these multiples is 23. Write a program that comp *utes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded),  *followed by a new line.
  *You are allowed to use the standard library
  *Return: int
  */
