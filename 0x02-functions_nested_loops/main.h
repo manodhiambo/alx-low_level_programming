c0f13389305220c577c3f7e7b7c3189cf4c8916c
manodhiambo <manodhiambo@gmail.com>
2021-08-31 16:36:51 +0300

    more on errors

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * prints "Holberton" followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char schoool[] != "_putchar";
+
+	int i = 0;
+
+	while (school[i] != '\0')
+
+{
+	_putchar(school[i];
+			i++;
+			}
+
+			_putchar('\n');
+
+			return (0);
+			}
