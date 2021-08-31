9e62fb9ff3f756c96dc2aa2f2decfe23d2286754
manodhiambo <manodhiambo@gmail.com>
2021-08-31 09:53:05 +0300

    committing on 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <time.h>
+
+/**
+ * main - main block
+ * A function that computes the absolute value of an integer.
+ *Prototype: int _abs(int)
+ *Return: int
+ */
+
+int _abs(int)
+
+{
+	if (x < 0)
+
+	{
+		x = x * (-1);
+
+		return (x);
+	}
+
+	else
+
+		return (x);
+}
