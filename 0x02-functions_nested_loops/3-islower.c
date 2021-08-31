94135bb1c39df1a1b0f5e0aab28ddc2586507122
manodhiambo <manodhiambo@gmail.com>
2021-08-31 09:18:50 +0300

    committing on 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * main - main block
+ * Write a function that checks for lowercase character.
+ * Prototype: int _islower(int c);
+ *Returns 1 if c is lowercase
+ *Returns 0 otherwise
+ *Return: int.
+ */
+
+ int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
