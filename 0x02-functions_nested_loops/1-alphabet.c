89e96f14592342154f25e07e62e087cda5a17ec6
manodhiambo <manodhiambo@gmail.com>
2021-08-31 18:38:15 +0300

    final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
 
 /**
  * main - main block
- *A function that prints the alphabet, in lowercase, followed by a new line
+ *Description - a program that prints the alphabet, in lowercase, followed by a  *new line
  *Prototype: void print_alphabet(void);
  *You can only use _putchar twice in your code
  *
  *Return: Always 0.
  */
-void print_alphabet(void)/main(void) 
+void print_alphabet(void)  
 
 {
 	int i;
