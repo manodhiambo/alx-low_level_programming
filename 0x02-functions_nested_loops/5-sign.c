566e801bd07231b71760a8e5bf18cb2e9a4b5e6c
manodhiambo <manodhiambo@gmail.com>
2021-08-31 09:43:15 +0300

    committing on 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <time.h>
+#include "holberton"
+
+/**
+ * main - main block
+ * A function that prints the sign of a number.
+ *Prototype: int print_sign(int n);
+ *Returns 1 and prints + if n is greater than zero
+ *Returns 0 and prints 0 if n is zero
+ *Returns -1 and prints - if n is less than zero
+ *Return: int
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+
+	{
+		_putchar(43);
+
+		return(1);
+	}
+
+	else if (n < 0)
+
+	{
+		_putchar(45);
+
+		return (-1);
+	}
+
+	else
+
+	{
+		_putchar(48);
+
+		return (0);
+	}
+
+}
