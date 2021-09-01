52be220d9e126317a66bc2703d9e2d8250f2e134
manodhiambo <manodhiambo@gmail.com>
2021-09-01 18:06:44 +0300

    holberton

---
+++ 0x03-debugging/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - this string prints Holberton
+ *
+ *Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	char c [9] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(c[i]);
+	_putchar('\n');
+	return (0);
+}
