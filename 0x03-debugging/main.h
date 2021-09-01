381a67c1bc8492d1cb1fb646c900402af55da1af
manodhiambo <manodhiambo@gmail.com>
2021-09-01 13:31:46 +0300

    main.h file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* main - Entry point
+* write a program tha prints Holberton, followed by a new line.
+* Return: Always (Success)
+*/
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+return (0);
