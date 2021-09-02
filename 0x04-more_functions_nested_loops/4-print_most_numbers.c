e4bb484307a03f7bdfd19c2d7254d61109723fd3
manodhiambo <manodhiambo@gmail.com>
2021-09-02 23:15:40 +0300

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,9 @@
 #include "holberton.h"
-#include <stdio.h>
-#include "main.h"
 
 /**
- * main - main block
+ * print_most_numbers - function that prints the numbers, from 0 to 9.
  *
- * A function that prints the numbers, from 0 to 9, followed by a new line
- * Do not print 2 and 4
- * You can only use _putchar twice in your code
- * Return: nothing
+ * Return: nothing.
  */
 
 void print_most_numbers(void)
@@ -18,8 +13,8 @@ void print_most_numbers(void)
 	while (n < 10)
 	{
 		if (n != 2 && n != 4)
-		_putchar(n + '0');
-	n += 1;
+			_putchar(n + '0');
+		n += 1;
 	}
 	_putchar('\n');
 }
