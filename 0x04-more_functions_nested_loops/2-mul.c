abf45f38d2f535a9b5961b40fbab317a2a9118f3
manodhiambo <manodhiambo@gmail.com>
2021-09-02 08:24:26 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stio.h>
+#include "holberton.h"
+#include "main.h"
+
+/**
+ * main - main block
+ *
+ * A function that multiplies two integers
+ * Prototype: int mul(int a, int b)
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
