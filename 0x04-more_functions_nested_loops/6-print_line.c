8c8df73a95f5139b930eaf58b782727aebf224f1
manodhiambo <manodhiambo@gmail.com>
2021-09-03 11:23:59 +0300

    committing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,24 @@
-#include "holberton.h"
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_line - function that draws a straight line in the terminal.
- * @n: input value to check
- * Return: nothing.
- */
+ *print_line - prints straight line
+ *@n: integer
+ *Return: no return value
+*/
 
 void print_line(int n)
 {
-	int i = 0;
+        char i;
 
-	while (i < n)
-	{
-		if (n > 0)
-		{
-			putchar(95);
-			i += 1;
-		}
-		else
-			_putchar('\n');
-	}
-	_putchar('\n');
+        if (n <= 0)
+        {
+                _putchar('\n');
+        }
+
+        for (i = 1; i <= n; i++)
+        {
+                putchar('');
+        }
+        _putchar('\n');
+
 }
