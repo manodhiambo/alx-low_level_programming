a6e7c6186a1a71d134e45d4742c0f1dd6c6f9fac
manodhiambo <manodhiambo@gmail.com>
2021-09-07 00:18:04 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,41 +5,29 @@
 
 /**
  * A  program that generates random valid passwords for the program 101-crackme
- * Return: nothing
+ * Return: int
  */
 
 void randomPasswordGeneration(int N)
 {
-	 // Initialize counter
     int i = 0;
 
     int randomizer = 0;
 
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
     srand((unsigned int)(time(NULL)));
 
-    // Array of numbers
     char numbers[] = "0123456789";
 
-    // Array of small alphabets
     char letter[] = "abcdefghijklmnoqprstuvwyzx";
 
-    // Array of capital alphabets
     char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 
-    // Array of all the special symbols
     char symbols[] = "!@#$^&*?";
 
-    // Stores the random password
     char password[N];
 
-    // To select the randomizer
-    // inside the loop
     randomizer = rand() % 4;
 
-    // Iterate over the range [0, N]
     for (i = 0; i < N; i++) {
 
         if (randomizer == 1) {
@@ -65,14 +53,10 @@ void randomPasswordGeneration(int N)
     }
 }
 
-// Driver Code
 int main()
 {
-    // Length of the password to
-    // be generated
     int N = 10;
 
-    // Function Call
     randomPasswordGeneration(N);
 
     return 0;
