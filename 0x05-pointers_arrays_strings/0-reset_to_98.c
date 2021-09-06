022c00a25762859f3003163233a8f1861d899aa1
manodhiambo <manodhiambo@gmail.com>
2021-09-06 07:22:41 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+#include "main.h"
+
+/**
+ * main - main block
+ * A funct takes pointer to an int as parameter & upd value it points to to 98
+ * Return: nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is based on pointers,arrays and strings
