ee5615dc629a15e980343b0492ddde69a16f40db
manodhiambo <manodhiambo@gmail.com>
2021-09-07 00:28:32 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,26 +10,38 @@
 
 void randomPasswordGeneration(int N)
 {
+    // Initialize counter
     int i = 0;
-
+  
     int randomizer = 0;
-
+  
+    // Seed the random-number generator
+    // with current time so that the
+    // numbers will be different every time
     srand((unsigned int)(time(NULL)));
-
-    int numbers[] = "0123456789";
-
+  
+    // Array of numbers
+    char numbers[] = "0123456789";
+  
+    // Array of small alphabets
     char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
+  
+    // Array of capital alphabets
     char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
+  
+    // Array of all the special symbols
     char symbols[] = "!@#$^&*?";
-
-    int password[N];
-
+  
+    // Stores the random password
+    char password[N];
+  
+    // To select the randomizer
+    // inside the loop
     randomizer = rand() % 4;
-
+  
+    // Iterate over the range [0, N]
     for (i = 0; i < N; i++) {
-
+  
         if (randomizer == 1) {
             password[i] = numbers[rand() % 10];
             randomizer = rand() % 4;
@@ -52,12 +64,16 @@ void randomPasswordGeneration(int N)
         }
     }
 }
-
+  
+// Driver Code
 int main()
 {
+    // Length of the password to
+    // be generated
     int N = 10;
-
+  
+    // Function Call
     randomPasswordGeneration(N);
-
+  
     return 0;
 }
