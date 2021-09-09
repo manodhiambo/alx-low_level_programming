0950a097e31deeeeaa5d74b346b7e150c3f2ef87
manodhiambo <manodhiambo@gmail.com>
2021-09-09 15:30:06 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,25 +2,22 @@
 
 /**
  * A  function that prints an integer
- * Prototype: void print_number(int n);
- * You can only use _putchar function to print
- * You are not allowed to use long
- * You are not allowed to use arrays or pointers
- * You are not allowed to hard-code special values
  * Return: int
  */
 
 void print_number(int n)
 {
-	 int n = 0;
-    while(n <= 99)
-    {
-        if(n < 10)
-        {
-            putchar((char)i/10 + '0');
-            putchar((char)n%10 + '0');
-            putchar('\n');
-        }
-        n++;
+
+    if (n < 0) {
+        putchar('-');
+        n = -n;
     }
+
+    if (n == 0)
+        putchar('0');
+
+    if (n/10)
+        print_number(n/10);
+
+    putchar(n%10 + '0');
 }
