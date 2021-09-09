0ffe8a3ada8134584348174779337271dac31f8d
manodhiambo <manodhiambo@gmail.com>
2021-09-09 15:17:28 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * A  function that prints an integer
@@ -12,16 +13,15 @@
 
 void print_number(int n)
 {
-	if (n < 0) {
-        putchar('-');
-        n = -n;
+	 int n = 0;
+    while(n <= 99)
+    {
+        if(n < 10)
+        {
+            putchar((char)i/10 + '0');
+            putchar((char)n%10 + '0');
+            putchar('\n');
+        }
+        n++;
     }
-
-    if (n == 0)
-        putchar('0');
-
-    if (n/10)
-        printnumber(n/10);
-
-    putchar(n%10 + '0');
 }
