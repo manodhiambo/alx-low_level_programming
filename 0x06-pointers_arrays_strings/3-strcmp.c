02943f5ef817ae3c5f494a38cdedb0bf071e128b
manodhiambo <manodhiambo@gmail.com>
2021-09-09 07:19:23 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
-#include "main.h"
 
 /**
- * A function that compares two strings
- * Your function should work exactly like strcmp
- * Return: char
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: pointer to char source 1
+ * @s2: pointer to char source 2
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
