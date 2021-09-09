c00587c8380f64144485a858e76e4bbfc2c2dd26
manodhiambo <manodhiambo@gmail.com>
2021-09-09 15:37:34 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * A  function that prints an integer
@@ -7,17 +8,23 @@
 
 void print_number(int n)
 {
+	if (n < 0) {
+       putchar('-');
+       n = -n;
+   }
 
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
+   if (n == 0)
+      putchar('0');
 
-    if (n == 0)
-        putchar('0');
+   if (n/10)
+      print_number(n/10);
 
-    if (n/10)
-        print_number(n/10);
+   putchar(n%10 + '0');
+}
 
-    putchar(n%10 + '0');
+int main(int argc, char** argv)
+{
+   int n = atoi(argv[1]);
+   print_number(n);
+   printf("\n");
 }
