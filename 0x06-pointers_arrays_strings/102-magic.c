43b42cbf914fbf87b25a4f2258f372688f5b338e
manodhiambo <manodhiambo@gmail.com>
2021-09-09 08:06:34 +0300

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "holberton.h"
 
 int main(void)
 {
@@ -19,6 +18,8 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
+	{
 	printf("a[2] = %d\n", a[2]);
+	}
 	return (0);
 }
