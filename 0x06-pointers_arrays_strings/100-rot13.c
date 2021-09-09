a307b258cfb6c0e885066cec7e25737366f9b080
manodhiambo <manodhiambo@gmail.com>
2021-09-09 07:40:19 +0300

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,38 +1,31 @@
 #include "holberton.h"
-#include "main.h"
 
 /**
- * A function that encodes a string using rot13.
- * Prototype: char *rot13(char *);
- * You can only use if statement once in your code
- * You can only use two loops in your code
- * You are not allowed to use switch
- * You are not allowed to use any ternary operation
+ * rot13 - function that encodes a string using rot13.
+ * @s: pointer to char
  * Return: char
  */
 
-char *rot13(char *)
+char *rot13(char *s)
 {
-	if('a' <= char * && char * <= 'z'){
-    return *rot13b(char *,'a');
-  } else if ('A' <= char * && char * <= 'Z') {
-    return *rot13b(char *, 'A');
-  } else {
-    return char *;
-  }
-}
+	int i = 0;
+	int j = 0;
+	char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
-char *rot13b(char *, char * basis){
-  c = (((char *-basis)+13)%26)+basis;
-  return char *;
-}
-
-char * main() {
-  printf("The given args will be rotated");
-  char *;
-  while((char * = getchar()) != EOF){
-    char * = *rot13(char *);
-    putchar(char *);
-  }
-  return 0;
+	while (*(s + i) != '\0')
+	{
+		while (*(l + j) != '\0')
+		{
+			if (*(s + i) == *(l + j))
+			{
+				*(s + i)  = *(r13 + j);
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (s);
 }
