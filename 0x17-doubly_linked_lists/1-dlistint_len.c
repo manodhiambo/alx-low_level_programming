ddf7152c80ebfb79077de2852d2349e837b80e1e
manodhiambo <manodhiambo@gmail.com>
2021-11-25 12:19:35 +0300

    fix 1-dlistint

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,12 @@
  * Return: the number of nodes
  */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t cont = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		cont++;
 	}
